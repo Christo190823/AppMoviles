77a72c8b1c18c57e29d65387780ed049faf77064
unknown <ah5008034@gmail.com>
2024-10-09 20:47:03 -0600

    Clase 09-10-24

--- Clase07-10-24/script.js
+++ Clase07-10-24/script.js
@@ -1,19 +1,19 @@
 
 
-
+//despliega lo señalado en el display
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
-
+//elimina los elementos del display
 function claerDisplay() {
     document.getElementById('display').value = '';
 }
-
+//Borra solo un digito del display
 function deleteLast(value) {
     let displayValue = document.getElementById('display').value;
     document.getElementById('display').value = displayValue.slice(0, -1);
 }
-
+//Comenta errores y hace las operacioness indicadas
 function calculateResult() {
     try {
         let result = eval(document.getElementById('display').value);
